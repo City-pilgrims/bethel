e97cab0b49b49bcaf2db5bf0f5f155f2e21b373d
citypilgrims <phdyongeun@gmail.com>
2025-06-12 01:57:18 +0900

    bethel_ver2 swarm rev base&deploy.py

--- bethel_v2/settings/base.py
+++ bethel_v2/settings/base.py
@@ -17,21 +17,6 @@ import os, environ
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
-# Reading .env file
-environ.Env.read_env(
-    env_file = os.path.join(BASE_DIR,'.env')
-)
-# Quick-start development settingss - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
 # Application definition
 
 INSTALLED_APPS = [
--- bethel_v2/settings/deploy.py
+++ bethel_v2/settings/deploy.py
@@ -1,7 +1,30 @@
+# base.py의 모든 것을 가져오는 것
 from .base import *
 
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Reading .env file
+environ.Env.read_env(
+    env_file = os.path.join(BASE_DIR,'.env')
+)
+# Quick-start development settingss - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['citypilgrims.com', 'www.citypilgrims.com']
 
@@ -12,10 +35,10 @@ ALLOWED_HOSTS = ['citypilgrims.com', 'www.citypilgrims.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST', default='mariadb'),
-        'PORT': env('DB_PORT', default='3306'),
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': read_secret('MYSQL_PASSWORD'),
+        'HOST': 'mariadb',
+        'PORT': '3306',
     }
 }
