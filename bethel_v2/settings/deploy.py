c464d00420e84d324ecf517e68415dc767a357fe
citypilgrims <phdyongeun@gmail.com>
2025-06-12 18:06:14 +0900

    bether ver2 deploy proxy ssl add

--- bethel_v2/settings/deploy.py
+++ bethel_v2/settings/deploy.py
@@ -26,7 +26,12 @@ SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['citypilgrims.com', 'www.citypilgrims.com']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+CSRF_COOKIE_SECURE = True   # HTTPS 환경에서는 반드시 True
+SESSION_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = ['citypilgrims.com', 'www.citypilgrims.com', '64.176.228.128']
 
 
 # Database
